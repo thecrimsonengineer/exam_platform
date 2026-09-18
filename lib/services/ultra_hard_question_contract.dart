2202b57fb89beec528ae98363d137dba0b5efe22
thecrimsonengineer <naveedhsse@gmail.com>
2026-09-18 08:41:18 +0530

    Add ultra hard question classification contract

---
+++ lib/services/ultra_hard_question_contract.dart
@@ -0,0 +1,8 @@
+class UltraHardQuestionContract {
+  const UltraHardQuestionContract._();
+
+  static const String classificationTag = 'ultra-hard-dqg300';
+  static const String displayName = 'Ultra Hard';
+  static const String practiceTitle = 'Exam Readiness';
+  static const String validatorLabel = 'DQG300 • 300/300 • DQS 100';
+}
