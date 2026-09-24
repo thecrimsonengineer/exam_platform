b59bcdaf91e90d44df096d741babae1b1317618e
thecrimsonengineer <naveedhsse@gmail.com>
2026-09-24 11:41:43 +0530

    FCP-2B: allow validating active competency state

--- test/features/flashcards/production/fcp_d02_run_model_test.dart
+++ test/features/flashcards/production/fcp_d02_run_model_test.dart
@@ -53,7 +53,7 @@ void main() {
         .map((item) => Map<String, dynamic>.from(item as Map))
         .toList();
     expect(competencies.first['status'], 'closed');
-    expect(competencies[1]['status'], 'in_progress');
+    expect(<String>{'in_progress', 'validating'}, contains(competencies[1]['status']));
     expect(
       competencies.skip(2).every((item) => item['status'] == 'not_started'),
       isTrue,
